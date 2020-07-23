9b2a57089a47b25123643bab06c4a4795b033ca5
Minroe <scott.whitney334@gmail.com>
2020-07-23 13:21:42 -0700

    made shop components reusable with link url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -7,8 +7,9 @@ import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
     <div className="App">
-      <Route exact path ='/' component={HomePage}/>
-     
+      <Switch>
+        <Route exact path ='/' component={HomePage}/>
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,7 @@ import './homepage.styles.scss'
 
 const HomePage = () => (
   <div className="homepage">
-    <Directory/>
+    <Directory />
     
   </div>
 );
