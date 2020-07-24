7b95a0636bc3a269022056f69890f68d6f196101
Minroe <scott.whitney334@gmail.com>
2020-07-24 15:23:18 -0700

    wokring on header/styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path ='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
