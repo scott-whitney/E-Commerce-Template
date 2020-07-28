395bb3ad3bb147faca7c71e3687bcb85b297e941
Minroe <scott.whitney334@gmail.com>
2020-07-28 16:58:18 -0700

    added the beginings of sign-in

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-signup/sign-in-and-sign-up.component';
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path ='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
